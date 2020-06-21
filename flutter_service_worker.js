edb6b5d30e3a9a759813208a76d35f9e3df329d4
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2020-06-21 16:48:12 -0300

    Button opens the drawer and drawer items will pop drawer (refactor needed)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "9251702e591db46393ebd239cb1687f7",
 "/": "9251702e591db46393ebd239cb1687f7",
-"main.dart.js": "3b351b93054126a851d7dd910d5cda75",
+"main.dart.js": "476684bd411559a93c44ba4ce564f43d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
