9672b691041242bb69993e78614fb0bbb2d99bc7
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2020-06-22 13:18:26 -0300

    Fix the circular profile image

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "9251702e591db46393ebd239cb1687f7",
 "/": "9251702e591db46393ebd239cb1687f7",
-"main.dart.js": "476684bd411559a93c44ba4ce564f43d",
+"main.dart.js": "864bfb33528b94624eb73300e17f2016",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
