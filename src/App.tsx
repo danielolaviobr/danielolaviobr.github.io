da1a3e4c6ee4e7ac37248a730217b8ad711bc7d0
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2020-11-06 20:51:57 -0300

    New portfolio landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GitHub, Linkedin } from "react-feather";
+import TopBarProgress from "react-topbar-progress-indicator";
+import "./styles/home.css";
 
-import { HomeMain } from "./styles/home";
+TopBarProgress.config({
+  barColors: {
+    "0": "#54b689",
+    "1.0": "#70c29c",
+  },
+  shadowColor: "#54b689",
+  shadowBlur: 2,
+});
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => setIsLoading(false), []);
   return (
-    <HomeMain>
-      <div className="content-wrapper">
-        <h1>Under Construction</h1>
-        <h3>Download my CV</h3>
-        <a
-          className="resume"
-          href="https://drive.google.com/uc?export=download&id=1Uxu-eBFdtR4W43ed-MU5zMcVZjwHrao_"
-        >
-          Download
-        </a>
-        <div className="socials">
-          <a href="https://github.com/danielolaviobr" target="_blank">
-            <GitHub /> GitHub
+    <div className="background">
+      <div>{isLoading && <TopBarProgress />}</div>
+      <div className="title-wrapper">
+        <span className="title">Daniel Olavio</span>
+        <div>
+          <span className="color-title">full-stack </span>
+          <span className="title">developer</span>
+        </div>
+        <div className="button-wrapper">
+          <a
+            href="https://github.com/danielolaviobr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
           </a>
-          <a href="https://br.linkedin.com/in/daniel-olavio" target="_blank">
-            <Linkedin />
-            LinkedIn
+          <a
+            href="https://br.linkedin.com/in/daniel-olavio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Linkedin
           </a>
         </div>
       </div>
-    </HomeMain>
+    </div>
   );
 }
 
